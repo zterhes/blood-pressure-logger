05f01aa91fa05ae06c3785de74cd67a615e5a336
zterhes <zterhes1@gmail.com>
2023-12-27 01:07:02 +0100

    ok

--- app/api/save/route.ts
+++ app/api/save/route.ts
@@ -1,13 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { validateMeasurement } from "../validation";
 import { saveMeasurement } from "../service";
+import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   console.log("body:", body);
   try {
     const measurement = validateMeasurement(body);
-    const response = saveMeasurement(measurement);
+    // const response = saveMeasurement(measurement);
+    const response = await prisma.measurement.create({
+      data: {
+        diastolic: measurement.diastolic,
+        systolic: measurement.systolic,
+        heartRate: measurement.heartRate,
+        isSpecial: measurement.cause !== undefined,
+        cause: measurement.cause,
+        timeStamp: new Date(),
+      },
+    });
     console.log(response);
     return new NextResponse(null, { status: 200 });
   } catch (error) {
