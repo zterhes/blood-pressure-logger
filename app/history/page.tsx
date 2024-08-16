4af291016704cec59a7029fa5aee9916909f7e7d
zterhes <zterhes1@gmail.com>
2024-08-16 16:18:28 +0200

    fix history page design

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -4,18 +4,23 @@ import HistoryData from "@/components/HistoryData";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { fetchAll } from "../utils/apiService";
+import { Spinner } from "@nextui-org/react";
 
 const Page = () => {
   const { status, data, error } = useQuery({
     queryKey: [fetchAll.key],
     queryFn: fetchAll.fn,
   });
-  console.log("error: ", error);
-  console.log("data: ", data);
   return (
     <div className=" w-full text-center px-8 min-h-[80vh]">
       <div>
         {error && "Not able to fetch data. Check your internet connection"}
+      </div>
+
+      <div>
+        {status === "pending" && <Spinner color="secondary" />}
+        {status === "error" &&
+          "Not able to fetch data. Check your internet connection"}
       </div>
       {data &&
         data.map((value, i) => <HistoryData key={i} measurement={value} />)}
